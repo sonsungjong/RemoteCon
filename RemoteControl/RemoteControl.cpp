a35062959390a7c77c6693cf98db26455a70d1e0
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2022-09-05 08:53:59 +0900

    안쓰는 기능 제거

--- RemoteControl/RemoteControl.cpp
+++ RemoteControl/RemoteControl.cpp
@@ -32,34 +32,34 @@ void CRemoteControl::OnExe(int port, const char* ip)
 
 void CRemoteControl::OnKill(int port, const char* ip)
 {
-	WSADATA data;
-	WSAStartup(0x0202, &data);
+	//WSADATA data;
+	//WSAStartup(0x0202, &data);
 
-	SOCKET listen_socket = socket(AF_INET, SOCK_DGRAM, 0);
-	sockaddr_in addr_data = { AF_INET, htons(port), };
-	InetPton(AF_INET, ip, &addr_data.sin_addr.s_addr);
+	//SOCKET listen_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	//sockaddr_in addr_data = { AF_INET, htons(port), };
+	//InetPton(AF_INET, ip, &addr_data.sin_addr.s_addr);
 
-	sockaddr_in server;
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-	InetPton(AF_INET, ip, &server.sin_addr);
+	//sockaddr_in server;
+	//server.sin_family = AF_INET;
+	//server.sin_port = htons(port);
+	//InetPton(AF_INET, ip, &server.sin_addr);
 
-	SOCKET out = socket(AF_INET, SOCK_DGRAM, 0);
+	//SOCKET out = socket(AF_INET, SOCK_DGRAM, 0);
 
-	char udp_msg[256];
-	ZeroMemory(udp_msg, 256);
-	sprintf_s(udp_msg, 256, "k%s", ip);
-	sendto(out, udp_msg, 256, 0, (sockaddr*)&server, sizeof(server));
+	//char udp_msg[256];
+	//ZeroMemory(udp_msg, 256);
+	//sprintf_s(udp_msg, 256, "k%s", ip);
+	//sendto(out, udp_msg, 256, 0, (sockaddr*)&server, sizeof(server));
 
-	closesocket(listen_socket);
+	//closesocket(listen_socket);
 
-	WSACleanup();
+	//WSACleanup();
 }
 
 void CRemoteControl::OnShutDown(int port, const char* ip)
 {
 	WSADATA data;
-	WSAStartup(0x0202, &data);
+	(void)WSAStartup(0x0202, &data);
 
 	SOCKET listen_socket = socket(AF_INET, SOCK_DGRAM, 0);
 	sockaddr_in addr_data = { AF_INET, htons(port), };
